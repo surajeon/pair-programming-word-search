bc6aef52f69e5ac35a6f91118e1be941e2fd3a37
Sura Jeon <surajeon@gmail.com>
2021-05-06 02:58:38 +0000

    Pair Programming - Word Search

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,20 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
+const wordSearch = (letters, word) => {
+    const horizontalJoin = letters.map((ls) => ls.join(""));
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+      if (l.includes(word)) return true;
     }
-}
-
-module.exports = wordSearch
+  
+    let VerticalString = "";
+    for (let y = 0; y < letters[0].length; y++) {
+      for (let x = 0; x < letters.length; x++) {
+        VerticalString += letters[x][y];
+      }
+    }
+    if (VerticalString.includes(word)) {
+      return true;
+    }
+  return false; 
+  };
+  
+  module.exports = wordSearch;
+  
